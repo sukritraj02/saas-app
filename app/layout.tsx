8af7d8c21c97e91ddce4d85d1d853366e24bef2b
sukritraj02 <sukritt.raj@gmail.com>
2025-07-11 01:25:48 +0530

    CLERK AUTH

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
@@ -21,9 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}
-        </body>
+        <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
+          <Navbar />
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
